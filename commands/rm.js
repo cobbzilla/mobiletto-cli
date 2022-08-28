c37a00bcfeb512a0e2efd16464e76982d552ab9d
Jonathan Cobb <jonathan@kyuss.org>
2022-08-27 22:21:27 -0400

    rm command implemented

--- commands/rm.js
+++ commands/rm.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 const commander = require('commander')
+const {mountAndPath, connect, handleCliError} = require("../connections");
+const chalk = require("chalk");
 
 const program = new commander.Command()
 
@@ -8,8 +10,26 @@ const cmd_rm = program.command('rm')
     .option('-r, --recursive', 'remove files recursively')
     .option('-f, --force', 'continue even when errors occur')
     .argument('<path>', 'The path to remove')
-    .action((path, options) => {
-        console.log(`rm ${JSON.stringify(options)} path=${path}`)
+    .action((path, opts) => {
+        try {
+            const mp = mountAndPath(path)
+            connect(mp.mount)
+                .then(conn => conn.remove(mp.path, { recursive: opts.recursive, quiet: opts.force }))
+                .then((removed) => {
+                        if (removed) {
+                            let removedFiles = ''
+                            if (Array.isArray(removed)) {
+                                removed.forEach(f => removedFiles += f + '\n')
+                            } else {
+                                removedFiles = removed
+                            }
+                            console.log(chalk.greenBright('Removed: ') + chalk.whiteBright(removedFiles))
+                        }
+                    },
+                    (err) => { if (err) { handleCliError(err, program) } })
+        } catch (e) {
+            handleCliError(e, program)
+        }
     });
 
 module.exports = cmd_rm
